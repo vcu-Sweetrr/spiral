59b7779307ca4546c110cc1ce83c64bc0988cd24
vcu-Sweetrr <sweetrr@vcu.edu>
2021-04-15 02:32:23 -0400

    late submition

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,15 @@
 def spiralize(number):
-    return_value = 1
+    x = 1 # starting point and number we are at in spiral
+    counter = 0 # variable to keep track how many corners we are at
+    i = 2 # increments that get added to starting point
+    total = 0 # sum of the corners 
 
-    return return_value
+    while x <= number ** 2: # while c is still less than square of number (size of spiral)
+        total += x # number we are at gets added to total
+        x += i # go to next corner
+        counter += 1 # count of which corner we are at
+        if counter == 4: # if we make a full rotation arount the corners, got to next one
+            i += 2 # increment the space need to reach corners
+            counter = 0 # reset the rotation
+
+    return total
